dbc5ff08b7c93c0e2d7d29f1a18b0712124d2ec6
matusilva <viniciusmatheus.dev@gmail.com>
2022-09-18 23:05:59 -0300

    ajustes no fetch api

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -30,13 +30,13 @@ export function Game() {
   }
 
   async function getDiscordUser(adsId: string) {
-    fetch(`http://192.168.3.49:3333/ads/${adsId}/discord`)
+    fetch(`http://seuIp:port/ads/${adsId}/discord`)
       .then(response => response.json())
       .then(data => setDiscordDuoSelected(data.discord))
   }
 
   useEffect(() => {
-    fetch(`http://192.168.3.49:3333/games/${game.id}/ads`)
+    fetch(`http://seuIp:port/games/${game.id}/ads`)
       .then(response => response.json())
       .then(data => setDuos(data))
   }, [])
